e54d21e1d85fd0fa7c37eea91fd625b80ce39aaa
Eric Coutu <eric.coutu@gmail.com>
2016-11-28 00:52:54 -0500

    Playing w/ servo

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "Arduino.h"
 #include "LiquidCrystal.h"
+// #include "Servo.h"
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
@@ -8,13 +9,22 @@ const unsigned int TEMP_SENSOR_PIN = A0;
 const unsigned int VOLTAGE_SENSOR_PIN = A1;
 const float baseline_temp = 20.0;
 
+// unsigned int angle = 0;
+// Servo servo;
+
 void setup() {
   analogReference(EXTERNAL);
   Serial.begin(9600);
 
+  // relay
   pinMode(8, OUTPUT);
   digitalWrite(8, LOW);
 
+  // servo
+  // servo.attach(9);
+  // servo.write(angle);
+
+  // lcd
   lcd.begin(16, 2);
 }
 
@@ -45,6 +55,15 @@ void loop() {
   lcd.print(temp);
   lcd.print((char) 223);
   lcd.print("C");
+
+  // unsigned long time = millis();
+  // angle = (angle + 60) % 180;
+
+  // lcd.setCursor(0, 1);
+  // lcd.print(angle);
+  // lcd.print((char) 223);
+
+  // servo.write(angle);
 
   delay(1000);
 }
