871d77294bc3f2d0c6a14199fc644c0a7fee7122
Eric Coutu <eric.coutu@gmail.com>
2017-01-28 18:13:14 -0500

    Continue elixir tutorial

--- lib/thermostat/registry.ex
+++ lib/thermostat/registry.ex
@@ -7,7 +7,8 @@ defmodule Thermostat.Registry do
   Starts the registry with the given `name`.
   """
   def start_link(name) do
-    GenServer.start_link(__MODULE__, :ok, name: name)
+    # 1. Pass the name to GenServer's init
+    GenServer.start_link(__MODULE__, name, name: name)
   end
 
   @doc """
@@ -15,44 +16,54 @@ defmodule Thermostat.Registry do
 
   Returns `{:ok, pid}` if the bucket exists, `:error` otherwise.
   """
-  def lookup(server, name) do
-    GenServer.call(server, {:lookup, name})
+  def lookup(server, name) when is_atom(server) do
+    # 2. Lookup is now done directly in ETS, without accessing the server
+    case :ets.lookup(server, name) do
+      [{^name, pid}] -> {:ok, pid}
+      [] -> :error
+    end
   end
 
   @doc """
   Ensures there is a bucket associated to the given `name` in `server`.
   """
   def create(server, name) do
-    GenServer.cast(server, {:create, name})
+    GenServer.call(server, {:create, name})
+  end
+
+  @doc """
+  Stops the registry.
+  """
+  def stop(server) do
+    GenServer.stop(server)
   end
 
   ## Server Callbacks
 
-  def init(:ok) do
-    names = %{}
+  def init(table) do
+    # 3. We have replaced the names map by the ETS table
+    names = :ets.new(table, [:named_table, read_concurrency: true])
     refs  = %{}
     {:ok, {names, refs}}
   end
 
-  def handle_call({:lookup, name}, _from, {names, _} = state) do
-    {:reply, Map.fetch(names, name), state}
-  end
-
-  def handle_cast({:create, name}, {names, refs}) do
-    if Map.has_key?(names, name) do
-      {:noreply, {names, refs}}
-    else
-      {:ok, pid} = Thermostat.Bucket.Supervisor.start_bucket
-      ref = Process.monitor(pid)
-      refs = Map.put(refs, ref, name)
-      names = Map.put(names, name, pid)
-      {:noreply, {names, refs}}
+  def handle_call({:create, name}, _from, {names, refs}) do
+    case lookup(names, name) do
+      {:ok, pid} ->
+        {:reply, pid, {names, refs}}
+      :error ->
+        {:ok, pid} = Thermostat.Bucket.Supervisor.start_bucket
+        ref = Process.monitor(pid)
+        refs = Map.put(refs, ref, name)
+        :ets.insert(names, {name, pid})
+        {:reply, pid, {names, refs}}
     end
   end
 
   def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
+    # 6. Delete from the ETS table instead of the map
     {name, refs} = Map.pop(refs, ref)
-    names = Map.delete(names, name)
+    :ets.delete(names, name)
     {:noreply, {names, refs}}
   end
 
--- test/thermostat/registry_test.exs
+++ test/thermostat/registry_test.exs
@@ -2,8 +2,8 @@ defmodule Thermostat.RegistryTest do
   use ExUnit.Case, async: true
 
   setup context do
-    {:ok, registry} = Thermostat.Registry.start_link(context.test)
-    {:ok, registry: registry}
+    {:ok, _} = Thermostat.Registry.start_link(context.test)
+    {:ok, registry: context.test}
   end
 
   test "spawns buckets", %{registry: registry} do
@@ -20,20 +20,25 @@ defmodule Thermostat.RegistryTest do
     Thermostat.Registry.create(registry, "shopping")
     {:ok, bucket} = Thermostat.Registry.lookup(registry, "shopping")
     Agent.stop(bucket)
+
+    # Do a call to ensure the registry processed the DOWN message
+    _ = Thermostat.Registry.create(registry, "bogus")
     assert Thermostat.Registry.lookup(registry, "shopping") == :error
   end
-  
+
   test "removes bucket on crash", %{registry: registry} do
     Thermostat.Registry.create(registry, "shopping")
     {:ok, bucket} = Thermostat.Registry.lookup(registry, "shopping")
-  
-    # Stop the bucket with non-normal reason
+
+    # Kill the bucket and wait for the notification
     Process.exit(bucket, :shutdown)
-  
+
     # Wait until the bucket is dead
     ref = Process.monitor(bucket)
     assert_receive {:DOWN, ^ref, _, _, _}
-  
+
+    # Do a call to ensure the registry processed the DOWN message
+    _ = Thermostat.Registry.create(registry, "bogus")
     assert Thermostat.Registry.lookup(registry, "shopping") == :error
   end
 end
